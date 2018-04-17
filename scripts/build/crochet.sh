a99242d557e2d0bf5e3bcbcc2d5d5b159d143f16
Luis Pina <luis@luispina.me>
2018-04-17 15:42:16 -0400

    Fixed dacapo tradebeans

--- scripts/build/crochet.sh
+++ scripts/build/crochet.sh
@@ -2,15 +2,19 @@
 
 source ../paths.sh
 
-sudo apt-get install -y --force-yes maven build-essential
+sudo apt-get install -y --force-yes maven build-essential criu
 git clone $CROCHET_REPO $CROCHET_DIR
 pushd $CROCHET_DIR
 {
     git checkout $CROCHET_BRANCH
     export JAVA_HOME=$JDK8_DIR
-    echo $JAVA_HOME
     mvn install -DskipTests
     ./instrumentJRE.sh
     make libcriu.dylib
+
+    # Get JCE from Openjdk8 and instrument it
+    rm $CROCHET_DIR/target/jre-inst/jre/lib/jce.jar
+    $JDK8_DIR/bin/java -Xmx6g -jar $CROCHET_DIR/target/CRIJ-0.0.1-SNAPSHOT.jar /usr/lib/jvm/java-8-openjdk-amd64/jre/lib/jce.jar $CROCHET_DIR/target/jre-inst/jre/lib/
+
 }
 popd
